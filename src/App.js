b80417d2775c630c2161c1d2e25be1160bd2aef3
Sordot <tgevirtz@gmail.com>
2021-11-03 01:18:27 -0500

    added useEffect that compares cards

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import SingleCard from './SingleCard';
 
 //create cards array
@@ -16,6 +16,8 @@ function App() {
 
   const [cards, setCards] = useState([])
   const [turns, setTurns] = useState(0)
+  const [choiceOne, setChoiceOne] = useState(null)
+  const [choiceTwo, setChoiceTwo] = useState(null)
 
   //shuffle cards, first duplicate cards, randomize cards, assign random ids
   //fires on click for New Game button
@@ -34,14 +36,43 @@ function App() {
     setTurns(0)
 
   }
+
+  //track user choices
+  const handleChoice = (card) => {
+
+    //if choiceOne is null (false), setChoiceOne, if it has a value (true), setChoiceTwo
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+    
+  }
+
+  //compare choiceOne and choiceTwo, fires whenever a new card is chosen
+  useEffect(() => {
+
+    //if we've selected a pair of cards, compare them, reset turn
+    if (choiceOne && choiceTwo) {
+      if (choiceOne.src === choiceTwo.src) {
+        console.log('those cards match')
+        resetTurn()
+      } else {
+        console.log('those cards dont match')
+        resetTurn()
+      }
+    }
+  }, [choiceOne, choiceTwo])
+
+  const resetTurn = () => {
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setTurns(prevTurns => prevTurns + 1)
+  }
   
-  //map over the cards and create divs for the front and back of each
+  //map over the SingleCards and create divs for the front and back of each
   return <>
     <div className="App">
       <h1>Memory Master</h1>
       <button onClick={shuffleCards}>New Game!</button>
       <div className='card-grid'>
-            {cards.map(card => (<SingleCard key={card.id} card={card}/>))}
+            {cards.map(card => (<SingleCard key={card.id} card={card} handleChoice={handleChoice}/>))}
       </div>
     </div>
   </>
--- src/SingleCard.js
+++ src/SingleCard.js
@@ -1,13 +1,16 @@
 import './SingleCard.css'
 
-const SingleCard = ({card}) => {
+const SingleCard = ({card, handleChoice}) => {
 
+    const handleClick = () => {
+        handleChoice(card)
+    }
     
     return <>
             <div className='card'>
                 <div>
                     <img className='front' alt='card-front' src={card.src} />
-                    <img src='/img/cover.png' className='back' alt='card-back' />
+                    <img src='/img/cover.png' className='back' onClick={handleClick} alt='card-back' />
                 </div>
             </div>
            
